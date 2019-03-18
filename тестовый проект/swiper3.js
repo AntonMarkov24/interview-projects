95cac67e5f49c672691700d188125789be5cc58a
AntonMarkov24 <48083493+AntonMarkov24@users.noreply.github.com>
2019-03-18 18:05:07 +0500

    выпадают слайды

--- тестовый проект/swiper3.js
+++ тестовый проект/swiper3.js
@@ -1,4 +1,4 @@
- var swiper = new Swiper('.swiper-container', {
+ var swiper = new Swiper('.swiper-container3', {
         pagination: {
         el: '.swiper-pagination',
         clickable: true,
@@ -7,5 +7,3 @@
             },
           },
         });
-
-
