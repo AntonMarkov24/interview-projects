75a0348e1f02157f336547e1dade624de4f6f437
AntonMarkov24 <48083493+AntonMarkov24@users.noreply.github.com>
2019-03-21 21:26:35 +0500

    5

--- тестовый проект/popap.js
+++ тестовый проект/popap.js
@@ -1,9 +1,40 @@
-var PopapBtn = document.getElementById('PopapBtn');
-var PopapBtnStyle = window.getComputedStyle(PopapBtn);
-var trainingCourses_Popap = document.getElementById('trainingCourses_Popap');
-var trainingCourses_PopapStyle = window.getComputedStyle(trainingCourses_Popap);
+var PopapBtn_TrainingCourses = document.getElementById('PopapBtn_TrainingCourses');
+var Popap_TrainingCourses = document.getElementById('Popap_TrainingCourses');
 
-PopapBtn.addEventListener('click', function() {trainingCourses_Popap.classList.toggle("trainingCourses_PopapOpen");
+PopapBtn_TrainingCourses.addEventListener('click', function() {Popap_TrainingCourses.classList.toggle("PopapOpen")})
 
 
-					   })
+var PopapBtn_capmuses = document.getElementById('PopapBtn_capmuses');
+var Popap_capmuses = document.getElementById('Popap_capmuses');
+
+PopapBtn_capmuses.addEventListener('click', function() {Popap_capmuses.classList.toggle("PopapOpen")})
+
+
+var PopapBtn_directions = document.getElementById('PopapBtn_directions');
+var Popap_directions = document.getElementById('Popap_directions');
+
+PopapBtn_directions.addEventListener('click', function() {Popap_directions.classList.toggle("PopapOpen")})
+
+
+var PopapBtn_specialty = document.getElementById('PopapBtn_specialty');
+var Popap_specialty = document.getElementById('Popap_specialty');
+
+PopapBtn_specialty.addEventListener('click', function() {Popap_specialty.classList.toggle("PopapOpen")})
+
+
+var PopapBtn_campus = document.getElementById('PopapBtn_campus');
+var Popap_campus = document.getElementById('Popap_campus');
+
+PopapBtn_campus.addEventListener('click', function() {Popap_campus.classList.toggle("PopapOpen")})
+
+
+var PopapBtn_programs = document.getElementById('PopapBtn_programs');
+var Popap_programs = document.getElementById('Popap_programs');
+
+PopapBtn_programs.addEventListener('click', function() {Popap_programs.classList.toggle("PopapOpen")})
+
+
+var PopapBtn_language = document.getElementById('PopapBtn_language');
+var Popap_language = document.getElementById('Popap_language');
+
+PopapBtn_language.addEventListener('click', function() {Popap_language.classList.toggle("PopapOpen")})
