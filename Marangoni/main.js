e8f481ed7ccdc33e9e2aeef28ceaeff8c4294d2d
AntonMarkov24 <48083493+AntonMarkov24@users.noreply.github.com>
2019-03-24 16:19:16 +0500

    11

---
+++ Marangoni/main.js
@@ -0,0 +1,31 @@
+ var swiper = new Swiper('.swiper-container', {
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+          },
+        });
+
+ var swiper = new Swiper('.swiper-container2', {
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+          },
+        });
+
+ var swiper = new Swiper('.swiper-container3', {
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (index + 1) + '</span>';
+            },
+          },
+        });
+
+
--- тестовый проект/swiper3.js
+++
@@ -1,9 +0,0 @@
- var swiper = new Swiper('.swiper-container3', {
-        pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + '</span>';
-            },
-          },
-        });
